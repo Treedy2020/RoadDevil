3d2b706b93843f3c4252916a56117e62e6d98a2d
TreeDy <2032974@tongji.edu.cn>
2023-04-14 01:03:01 +0800

    Added the discription of JYL and the png file of courses.

--- 0_🌏_Home.py
+++ 0_🌏_Home.py
@@ -15,19 +15,21 @@ st.set_page_config(
 )
 
 with st.sidebar:
-    initial_d = Image.open('./data/Others/Initial_D_Arcade_Stage_5.webp')
+    initial_d = Image.open('data/Others/Initial_D_Logo.png')
     st.image(initial_d, caption='Initial D Arcade Stage 5', use_column_width=True)
     
+title, title_graph = st.columns(2)
 
-st.title('Road Devil' )
+with title_graph:
+    graph = Image.open('./data/Others/Initial_D_Arcade_Stage_5.webp')
+    st.image(graph, use_column_width=True)
+    
+with title:
+    st.title('Road Devil' )
 
 st.header('Discription')
-# annotated_text(
-#     "**Road Devil** is an",
-#     (" **Initial D Arcade Stage 5**",  "an arcade racing game"),
-#     "team constructed by `JYL` and `Yoh`, at first there were over 30 drivers from China, Sichuan. In 2012/07/21, they won the championship of the race against SSR team from ChengDu and become the best team of the The southwest of China."
-# )
-st.markdown("> **Road Devil** is an **Initial D Arcade Stage 5** `(an arcade racing game)` team constructed by `JYL` and `Yoh`, at first there were over 30 drivers from China, Sichuan. RD now mainly refers to the small team of five drivers who  represented Mianyang to participate in an intercity challenge in 2012/07/21. they won the championship of the race against SSR team from ChengDu and become the best team of the The southwest of China. ")
+
+st.markdown("### > **Road Devil** is an **Initial D Arcade Stage 5** `(an arcade racing game)` team constructed by `JYL` and `YOH`, at first there were over 30 drivers from China, Sichuan. RD now mainly refers to the small team of five drivers who  represented Mianyang to participate in an intercity challenge in 2012/07/21. they won the championship of the race against SSR team from ChengDu and become the best team of the The southwest of China. ")
 
 # We could use a decorator like the next line to acceleate this
 @st.cache_data
@@ -43,38 +45,68 @@ st.header('Drivers')
 driver1, driver2, driver3, driver4, driver5 = st.tabs(['TIGA', 'YOH', 'JYL', 'TONG', 'XIAO'],)
 
 with driver1:
-    tiga = Image.open('./data/driver_photos/tiga.jpg')
-    photo, discription = st.columns(2)
+    tiga, ae86 = Image.open('./data/driver_photos/tiga.jpg'), Image.open('./data/Others/ae86.png')
+    akina_1, akina_2 = Image.open('data/course/akina_01.png'), Image.open('data/course/akina_02.png')
+    photo, discription, course = st.columns(3)
+    
     with photo:
-        st.image(tiga, width=250)
+        st.image(tiga, use_column_width=True)
+        
+        
     with discription:
-        st.info('## The best time attack driver of RD.')
-        st.success('**Car**:     TRUENO 3door GT-APEX(AE86)')
-        st.success('**Home Course**:     秋名 (Akina)')
-        st.markdown("> **Discription**:\n `TIGA` was initially noticed by `JYL` for his awsome Akina downhill, he is one of **RD's Double ACE** and **The Time Attacker**. He was called `Takumi` due to his youngest age. He currently work in Shanghai and his spare time interest is artificial intelligence technology.")
+        st.info('## The best Time Attacker of RD')
+        st.success('#### **Car**:     TRUENO 3door GT-APEX(AE86)')
+        st.image(ae86,use_column_width=True)
+        st.markdown("####  **Discription**:\n #### `TIGA` was initially noticed by `JYL` for his awsome Akina downhill, he is one of **RD's Double ACE** and **The Time Attacker**. He was called `Takumi` due to his youngest age. Now he works in Shanghai and his spare time interest is artificial intelligence technology.")
+    
+    with course:
+        st.success('#### **Home Course**:     秋名 (Akina)')
+        st.image(akina_1, use_column_width=True)
+        st.image(akina_2, use_column_width=True)
+        
         
 
 with driver2:
-    yoh = Image.open('./data/driver_photos/yoh.jpg')
-    photo, discription = st.columns(2)
+    yoh, cap = Image.open('./data/driver_photos/yoh.jpg'), Image.open('./data/Others/cap.png')
+    irohazaka_01, irohazaka_02 = Image.open('data/course/irohazaka_01.png'), Image.open('data/course/irohazaka_02.png')
+    photo, discription, course = st.columns(3)
+    
     with photo:
-        st.image(yoh, width=250)
+        st.image(yoh, use_column_width=True)
+        
     with discription:
-        st.info('## The leader and mind of RD.')
-        st.success('**Car**: Suzuki Cappuccino')
-        st.success('**Home Course**: いろは坂 (Irohazaka)')
-        st.markdown('> **Discription**: Brain of RD. In 2009-2010, `YOH` was one of the strongest driver, famous in the D5 racing world in Mianyang. He and his red Cappuccino held the Inohazaka downhill record for a long time, `YOH` loves Go in his spare time,  he now has a cute daughter who .')
+        st.info('## The leader and mind of RD')
+        st.success('#### **Car**: Suzuki Cappuccino')
+        st.image(cap, use_column_width=True)
+        st.markdown('#### **Discription**:\n #### Brain of RD. In 2009-2010, `YOH` was one of the strongest driver, famous in the D5 racing world in Mianyang. He and his red Cappuccino held the Inohazaka downhill record for a long time, `YOH` loves Go in his spare time,  he now has a cute and lovely daughter who is in primary school.')
+        
+    with course:
+        st.success('#### **Home Course**: いろは坂 (Irohazaka)')
+        st.image(irohazaka_01, use_column_width=True)
+        st.image(irohazaka_02, use_column_width=True)
+        
 
 with driver3:
-    jyl = Image.open('./data/driver_photos/yoh.jpg')
-    photo, discription = st.columns(2)
+    jyl, fd6 = Image.open('./data/driver_photos/jyl.jpg'), Image.open('data/Others/fd6.png')
+    happogahara_01, happogahara_02 = Image.open('./data/course/happogahara_01.png'), Image.open('./data/course/happogahara_02.png')
+    photo, discription, course = st.columns(3)
+    
     with photo:
-        st.image(jyl, width=250)
+        st.image(jyl, use_column_width=True)
+        
     with discription:
-        st.info('## The leader and mind of RD.')
-        st.success('**Car**: Suzuki Cappuccino')
-        st.success('**Home Course**: いろは坂 (Irohazaka)')
-        st.markdown('> **Discription**: Brain of RD. In 2009-2010, `YOH` was one of the strongest driver, famous in the D5 racing world in Mianyang. He and his red Cappuccino held the Inohazaka downhill record for a long time, `YOH` loves Go in his spare time,  he now has a cute daughter who .')
+        st.info('## The best battler of RD.')
+        st.success('#### **Car**: RX-7 Type RS [FD3S]')
+        st.image(fd6)
+        st.markdown("#### **Discription**: Frontier of initial D in Mianyang, has been recognized as the strongest character for a long time. `JYL` likes compound courses involving uphill and downhill. As the first member who take the field, he won two important races at happogahara in Chengdu in the against game.")
+    
+    with course:
+        st.success('#### **Home Course**: 八方ヶ原 (Happogahara)')
+        st.image(happogahara_01, use_column_width=True)
+        st.image(happogahara_02, use_column_width=True)
+        
+        
+        
     
 
 st.header('Group Photo')
@@ -82,7 +114,7 @@ st.header('Group Photo')
 image = Image.open('./data/4drivers.jpg')
 st.image(image, caption='Tong JYL TIGA XIAO', use_column_width=True)
 
-# GPT Part
+# Advice Part
 st.header("Any advice for this page?")
 
 comment = st.text_area('')
@@ -92,6 +124,8 @@ if enter:
     st.markdown('Hello, this is ChatGPT!')
 else:
     st.write("We have gpt-3.5-turbo to be a chatbot.")
+
+
 
 
 
